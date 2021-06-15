7b3a3fb9f9038b470e64426d55d82fe185e62864
clayton.oliveira <clayton.oliveira@INV009213.spread.tecnologia>
2021-06-15 01:06:16 -0300

    ajuste codigo

--- src/test/java/br/com/swapi/api/test/AbstractTest.java
+++ src/test/java/br/com/swapi/api/test/AbstractTest.java
@@ -6,7 +6,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 /**
- * Classe Abstrata de Teste para carregar informações do Springboot
+ * Classe Abstrata de Teste para carregar informaÃ§Ãµes do Springboot
  *  
  * @author Clayton Morais de Oliveira
  *
